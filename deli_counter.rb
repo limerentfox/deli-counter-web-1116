8c838a8ebe0b097dd7efbfd900f4415f5f7440b6
limerentfox <christi@limerentfox.com>
2016-10-31 02:56:31 -0400

    deli-counter lab complete

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+
+
+def line(katz_deli)
+  line = ""
+  katz_deli.each_with_index do |name, i| 
+    line += (" #{i + 1}. #{name}")
+  end
+    if katz_deli.size == 0
+      puts "The line is currently empty."
+    else
+      puts "The line is currently:" + line.to_s
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
+end
+
+
+def now_serving(katz_deli)
+  next_person = katz_deli.shift
+  if katz_deli.size == 0
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{next_person}."
+  end
+end
+
